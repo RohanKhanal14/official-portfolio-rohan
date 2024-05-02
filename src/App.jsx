39b6ec14be70b8ffa991dad7df4d2f48241f0ddf
Rohan <rohankhanal114@gmail.com>
2024-05-03 00:50:06 +0545

    final again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { About, Header, Skill, Work, Contact } from "./container";
 import "./App.scss";
 function App() {
